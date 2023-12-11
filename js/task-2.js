16ae57f5faf60e3b6156335dba05b3e3efde31e1
madedemo <madelka518@gmail.com>
2023-12-11 21:19:43 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,7 @@
 function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0;
+  }
   const totalCalories = days.reduce((acc, day) => acc + day.calories, 0);
   const averageCalories = totalCalories / days.length;
   return averageCalories;
